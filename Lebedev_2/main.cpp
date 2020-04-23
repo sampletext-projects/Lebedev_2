a688645b61b7d3de57e76bec8dbe96da6f1033dc
sampletext32 <sampletext32@bk.ru>
2020-04-23 15:44:52 +0300

    Update to steps 8

--- Lebedev_2/main.cpp
+++ Lebedev_2/main.cpp
@@ -5,16 +5,19 @@ int main()
 {
 	setlocale(LC_ALL, "russian");
 
+	cout << "Автор: Лебедев П.В. (УМЛ-112)\n";
+	cout << "Программа для вычисления функции" << endl;
+	
 	for (int x = -10; x <= 10; x++)
 	{
 		double chisl = 1;
 		double znamen = 1;
-		for (int i = 2; i <= 64; i += 2)
+		for (int i = 2; i <= 8; i += 2)
 		{
 			//проход чётных множителей
 			chisl *= x / 10. - i;
 		}
-		for (int i = 1; i <= 64; i += 2)
+		for (int i = 1; i <= 8; i += 2)
 		{
 			//проход нечётных множителей
 			znamen *= x / 10. - i;
